380cc6c9ef7bfbb3e217395c23d92c92db8d8b49
Keshav Mesta <kmesta@sapient.com>
2016-04-13 03:04:54 +0530

    Error handling for guest user & duplicate up votes

--- api/actions/topic/upvote.js
+++ api/actions/topic/upvote.js
@@ -5,29 +5,35 @@ import mongoose from 'mongoose';
 export default function upvote(req) {
 
   return new Promise((resolve, reject) => {
-    let topic_id = req.body._id;
-    let user = req.session.user.name || req.session.user || 'srahm3';
+    if(req.session.user === undefined) {
+      reject('You need to be logged in to upvote topics');
+    } else {
+      let topic_id = req.body._id;
+      let user = req.session.user.name;
 
-    // Check if user has already voted for the topic
-    UpvoteModel.find({user: user, topicId: topic_id}).then(function(result){
-      if(result.length===0) {
-        // make async call to database
-        TopicModel.update({_id: mongoose.Types.ObjectId(topic_id)}, {$inc: {upVotes: 1}}).then(function(result){
-          UpvoteModel.create({user: user, topicId: topic_id}).then(function(result) {
-            TopicModel.find({}).then(function(result) {
-              resolve(result);
+      // Check if user has already voted for the topic
+      UpvoteModel.find({user: user, topicId: topic_id}).then(function(result){
+        if(result.length===0) {
+          // make async call to database
+          TopicModel.update({_id: mongoose.Types.ObjectId(topic_id)}, {$inc: {upVotes: 1}}).then(function(result){
+            UpvoteModel.create({user: user, topicId: topic_id}).then(function(result) {
+              TopicModel.find({}).then(function(result) {
+                resolve(result);
+              }, function(err) {
+                reject(err);
+              })
             }, function(err) {
               reject(err);
             })
-          }, function(err) {
+          }, function(err){
             reject(err);
           })
-        }, function(err){
-          reject(err);
-        })
-      }
-    }, function(err){
-      reject(err);
-    });
+        } else {
+          reject('You have already upvoted this particular topic');
+        }
+      }, function(err){
+        reject(err);
+      });
+    }
   });
 }
