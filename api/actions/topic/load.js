985f03e14f0078d61dceec7b71f9f76e508f0b04
Keshav Mesta <kmesta@sapient.com>
2016-04-16 18:14:52 +0530

    label change

--- api/actions/topic/load.js
+++ api/actions/topic/load.js
@@ -4,12 +4,12 @@ import { UpvoteModel } from './resource/UpvoteModel';
 export default function load(req) {
   return new Promise((resolve, reject) => {
     // make async call to database
-    TopicModel.find({}).then(function(result){
+    TopicModel.find({status: 1}).then(function(result){
       let topics = result;
       let upvotedTopics = [];
 
       if(req.session.user) {
-        UpvoteModel.find({user: req.session.user.name}, {topicId: 1}).then(function(result) {
+        UpvoteModel.find({user: req.session.user.username}, {topicId: 1}).then(function(result) {
           if(result && result.length) {
             result.map((topic) => upvotedTopics.push(topic.topicId));
             resolve({topics: topics, upvotedTopics: upvotedTopics});
