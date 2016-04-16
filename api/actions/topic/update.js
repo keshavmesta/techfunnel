737e8d244ca2ffcea43f9f96e7a685759c7ee940
Keshav Mesta <kmesta@sapient.com>
2016-04-16 18:15:50 +0530

    added user details via service

--- api/actions/topic/update.js
+++ api/actions/topic/update.js
@@ -1,17 +1,23 @@
-import load from './load';
 import { TopicModel } from './resource/TopicModel';
 
 export default function update(req) {
+  let user = req.session.user;
   return new Promise((resolve, reject) => {
-    // write to database
-    const topics = load(req);
-    const topic = req.body;
-    topic.datePosted = Date();
-    topic.status = 1;
-    TopicModel.create(topic).then(function (result) {
-      resolve(result);
-    }, function (err) {
-      reject(err)
-    });
+    if(user) {
+      // write to database
+      const topic = req.body;
+      topic.speakerName = user.name;
+      topic.speakerId = user.oracleId;
+      topic.speakerEmail = user.email;
+      topic.datePosted = Date();
+      topic.status = 1;
+      TopicModel.create(topic).then(function (result) {
+        resolve(result);
+      }, function (err) {
+        reject(err)
+      });
+    } else {
+      reject('You need to be logged in to post topics');
+    }
   });
 }
