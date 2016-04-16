765e2bf404cd7c6528289797a3dd0136c84200a2
Keshav Mesta <kmesta@sapient.com>
2016-04-16 18:16:51 +0530

    plugged in service call to fetch user details

--- api/actions/login.js
+++ api/actions/login.js
@@ -1,10 +1,28 @@
 import request from 'request';
+import superagent from 'superagent';
 
 export default function login(req) {
-  const user = {
-    name: req.body.username,
-    token: req.body.token
-  };
-  req.session.user = user;
-  return Promise.resolve(user);
+  return new Promise((resolve, reject) => {
+    const user = {
+      username: req.body.username,
+      token: req.body.token
+    };
+    superagent
+      .post('http://studioauth.sapient.com/apiv1/verifyToken')
+      .set('x-access-token', user.token)
+      .end((err, res) => {
+        if (res.status === 200 && res.statusCode === 200) {
+          if(res.body.isValid) {
+            user.name = res.body.user.name;
+            user.email = res.body.user.email;
+            user.oracleId = res.body.user.oracle_id;
+            user.profilePic = res.body.user.profilePic;
+            req.session.user = user;
+            resolve(user);
+          }
+        } else {
+          reject(err);
+        }
+      })
+  });
 }
